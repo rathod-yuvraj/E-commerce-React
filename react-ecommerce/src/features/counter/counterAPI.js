e1ea671e71a2c81dfd2fb51087648409fb94464f
yuvrajrathod8390 <yuvrajrathod8390@gmail.com>
2024-02-22 17:13:51 +0530

    add frist file

--- react-ecommerce/src/features/counter/counterAPI.js
+++ react-ecommerce/src/features/counter/counterAPI.js
@@ -1,6 +1,13 @@
 // A mock function to mimic making an async request for data
+
+
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+  return new Promise( async (resolve) =>{
+
+  const response = await fetch ('http:localhost:8080')
+  const data= await response.json()
+  resolve({data})
+}
+    
   );
 }
