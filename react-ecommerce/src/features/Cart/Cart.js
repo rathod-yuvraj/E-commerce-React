d1c4955aa9e01a667e564541aa51246d9fbfda51
yuvrajrathod8390 <yuvrajrathod8390@gmail.com>
2024-04-13 22:46:48 +0530

    cartupdate

--- react-ecommerce/src/features/Cart/Cart.js
+++ react-ecommerce/src/features/Cart/Cart.js
@@ -13,6 +13,7 @@ const products = [
     color: 'Salmon',
     price: '$90.00',
     quantity: 1,
+
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
     imageAlt:
@@ -20,6 +21,17 @@ const products = [
   },
   {
     id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+  id: 3,
     name: 'Medium Stuff Satchel',
     href: '#',
     color: 'Blue',
