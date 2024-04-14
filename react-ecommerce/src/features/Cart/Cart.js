7b8ddd248b7a66db87fa927e6dff4561cc3e02a7
yuvrajrathod8390 <yuvrajrathod8390@gmail.com>
2024-04-14 23:47:42 +0530

    upadecart products

--- react-ecommerce/src/features/Cart/Cart.js
+++ react-ecommerce/src/features/Cart/Cart.js
@@ -31,7 +31,20 @@ const products = [
     imageAlt:
       'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
   },
-  id: 3,
+  {
+    id: 3,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+  {
+    id: 4,
     name: 'Medium Stuff Satchel',
     href: '#',
     color: 'Blue',
